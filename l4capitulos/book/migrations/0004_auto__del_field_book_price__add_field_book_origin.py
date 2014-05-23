d04ada73cb823499940edd69f774cf6630a44402
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2014-05-23 13:20:12 -0300

    Add migration dependencie

--- l4capitulos/book/migrations/0004_auto__del_field_book_price__add_field_book_origin.py
+++ l4capitulos/book/migrations/0004_auto__del_field_book_price__add_field_book_origin.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("finance", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Deleting field 'Book.price'
         db.delete_column(u'book_book', 'price')
