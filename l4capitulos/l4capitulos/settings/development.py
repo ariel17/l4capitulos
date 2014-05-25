27b1f26220df72a2e22004ca1c207db2360e9387
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2014-05-24 21:35:09 -0300

    Add development settings.

---
+++ l4capitulos/l4capitulos/settings/development.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Description: Production settings and globals.
+"""
+__author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
+
+
+import os
+from os import environ
+
+from base import *
+
+
+########## HOST CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
+ALLOWED_HOSTS = ['*']
+########## END HOST CONFIGURATION
+
+
+########## DATABASE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'l4capitulos',
+        'USER': 'l4capitulos',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+########## END DATABASE CONFIGURATION
+
+
+########## CACHE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'localhost:11211',
+        'KEY_PREFIX': 'production-',
+    },
+}
+########## END CACHE CONFIGURATION
+
+
+########## SECRET CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = get_env_setting('SECRET_KEY')
+########## END SECRET CONFIGURATION
+
+
+########## INSTALLED APPS
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
+INSTALLED_APPS += ('gunicorn',)
+########## END INSTALLED APPS
+
+
+MEDIA_URL = 'https://www.poppurri.com.ar/media/'
+STATIC_URL = 'https://www.poppurri.com.ar/static/'
+
+
+# Sentry client configuration
+RAVEN_CONFIG = {
+    'dsn': 'http://9f46b93862b2433e862151a904acaa8b:bb5759ac2e794d93a1c83b785924797a@sentry.ariel17.com.ar/10',
+}
+
+
+# vim: ai ts=4 sts=4 et sw=4 ft=python
