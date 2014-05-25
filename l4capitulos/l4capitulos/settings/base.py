fb1d381ffa7f498b9809d0bc07bfff2a99f992ab
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2014-05-24 22:59:02 -0300

    Remove secret key from base setting.

--- l4capitulos/l4capitulos/settings/base.py
+++ l4capitulos/l4capitulos/settings/base.py
@@ -32,9 +32,6 @@ BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), '..')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9u3yqc)=8!9h!s-bii$%pb^b*vg7u-%rtq!c5ad+-hxh1h)pt4'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
